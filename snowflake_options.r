fbe5de8df2394f5990ac2b9510f0c888f4489e70
Rick Saporta <rsaporta@gmail.com>
2015-05-13 18:09:11 -0400

    snowflake_options

---
+++ snowflake_options.r
@@ -0,0 +1,13 @@
+## SET THESE OPTIONS AS APPROPRIATE
+
+## Username
+options("snowflake_uid" = " . . ")
+
+## Password
+options("snowflake_pwd" = " . . ")
+
+## Default warehouse
+options("snowflake_defaultwh" = "LOAD_WH")
+
+## Flag to indicate whether snowflake is currently in use
+options("snowflake_inuse" = TRUE)
